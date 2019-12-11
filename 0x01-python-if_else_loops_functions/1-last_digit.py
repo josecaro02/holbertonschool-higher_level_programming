cc89cf39c6fec1e352e1ac4234e83bbb6effb761
josecaro02 <j.caro0207@gmail.com>
2019-12-11 00:52:40 +0000

    [Fixing mismatch results with numbers greater than 5]

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ if number < 0:
     print("-{:d} {}".format(n_number, str0) if n_number > 0 else "0 and is 0")
 else:
     p_number = number % 10
-    if p_number > 6:
-        str1 = "and is greater than 6 and not 0"
+    if p_number > 5:
+        str1 = "and is greater than 5"
     elif p_number > 0:
         str1 = "and is less than 6 and not 0"
     print("{:d} {}".format(p_number, str1) if p_number != 0 else "0 and is 0")
