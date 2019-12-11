8226220c2e98e489b5bb415209f51e345fd17e07
josecaro02 <j.caro0207@gmail.com>
2019-12-11 00:40:13 +0000

    [Adding shebang to tasks 7, 8, 9, 10, 11, 101 and fix 3 format task]

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def remove_char_at(str, n):
     phrase = ""
     num = 0
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     for i in str + "\n":
         if ord(i) > 96 and ord(i) < 123:
