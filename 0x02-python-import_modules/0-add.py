65fa9843b6354f3548ec193d505be2594e16fbec
Jose David Caro Cantor <j.caro0207@gmail.com>
2019-12-12 19:38:34 +0000

    [Adding contidion that adding only the function wanted to task 0]

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    add = add_0.add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, add))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
