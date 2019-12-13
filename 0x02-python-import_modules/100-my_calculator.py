bcd6228a327c3ff418da16e0e5239f3091d0bf65
josecaro02 <j.caro0207@gmail.com>
2019-12-13 00:22:29 +0000

    [Adding task 5 and 100]

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    argc = len(sys.argv)
+    ops = ['+', '-', '*', '/']
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        for i in range(4):
+            if ops[i] == sys.argv[2]:
+                if i == 0:
+                    res = a + b
+                elif i == 1:
+                    res = a - b
+                elif i == 2:
+                    res = a * b
+                else:
+                    res = a // b
+                print("{:d} {:s} {:d} = {:d}".format(a, sys.argv[2], b, res))
+                break
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
