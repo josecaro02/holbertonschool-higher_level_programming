cf22b13e7d614a7c50c757078e9628c54f979ab5
josecaro02 <j.caro0207@gmail.com>
2019-12-17 23:11:02 +0000

    Reform the code of task 6 to print the correct answer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,13 +3,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end = '')
-                if j < (len(matrix[i]) - 1):
-                    print(" ", end = '')
-                if j == (len(matrix[i]) - 1):
-                   print("")
-
-    else:
-        print("")
+        for i in matrix:
+            b_spc = 0
+            for j in i:
+                if b_spc != 0:
+                    print(" ", end='')
+                b_spc = 1
+                print("{:d}".format(j), end='')
+            print("")
