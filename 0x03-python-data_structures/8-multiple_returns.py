59d17b9ed4a3dcdf4fc6b2854351b40196bcf865
josecaro02 <j.caro0207@gmail.com>
2019-12-17 23:50:19 +0000

    [Fixing style and shebang]

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
-#!/usr/bin/py
+#!/usr/bin/python3
+
 
 def multiple_returns(sentence):
     if sentence:
-        return((len(sentence),sentence[0]))
+        return((len(sentence), sentence[0]))
     else:
         return(0, None)
