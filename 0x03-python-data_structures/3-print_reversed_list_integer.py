a1d10fc2962057d7e42c482c28dbad4143769519
josecaro02 <j.caro0207@gmail.com>
2019-12-17 18:26:23 +0000

    [Adding task 4 and modify task 3, do when list exists]

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) != 0:
+    if my_list:
         my_list.reverse()
         for i in my_list:
             print("{:d}".format(i))
