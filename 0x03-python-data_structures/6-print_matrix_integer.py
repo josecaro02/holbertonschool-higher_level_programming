f5068e9952e4bf6d599c0686b58bae4aa1afb9b8
josecaro02 <j.caro0207@gmail.com>
2019-12-17 22:53:46 +0000

    [Reupload task 6 checkink if matrix exists]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) != 1:
-        for i in range (len(matrix)):
-            for j in range (len(matrix[i])):
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end = '')
-                if j < len(matrix[i]) - 1:
+                if j < (len(matrix[i]) - 1):
                     print(" ", end = '')
-                if j == (len (matrix[i]) - 1):
+                if j == (len(matrix[i]) - 1):
                    print("")
 
     else:
