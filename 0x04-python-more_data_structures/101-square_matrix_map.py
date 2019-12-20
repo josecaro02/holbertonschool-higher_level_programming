e8d8a06d397442912cef64adb6038829f50bca66
josecaro02 <j.caro0207@gmail.com>
2019-12-20 00:06:58 +0000

    [Editing code to make it has 3 lines]

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    if matrix:
-        pos = 0
-        n_matrix = []
-        for i in matrix:
-            n_matrix.append(list(map(lambda x: x ** 2, i)))
-        return (n_matrix)
+    return (list(map(lambda x : (list(map(lambda y : y * y , x))), matrix)))
