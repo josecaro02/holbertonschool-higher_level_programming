2a8db4f8bcd65750e8a947e033c7c506936aee11
josecaro02 <j.caro0207@gmail.com>
2019-12-20 01:32:41 +0000

    [Delete if a_dictionary statement]

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,8 @@
 
 
 def complex_delete(a_dictionary, value):
-    if a_dictionary:
-        key_list = list(a_dictionary.items())
-        for i in key_list:
-            if value == i[1]:
-                del a_dictionary[i[0]]
-        return (a_dictionary)
+    key_list = list(a_dictionary.items())
+    for i in key_list:
+        if value == i[1]:
+            del a_dictionary[i[0]]
+    return (a_dictionary)
