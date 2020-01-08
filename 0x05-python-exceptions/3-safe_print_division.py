e7c26119b517ef157eed58ac2ba7691975e8c975
Jose David Caro <j.caro0207@gmail.com>
2020-01-08 18:19:50 +0000

    [3-safe print division now prints result in finally: section]

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,10 @@
 
 def safe_print_division(a, b):
     try:
-        print("Inside result: {:.1f}".format(a / b))
+        result = a / b
         return(a / b)
     except:
-        print("Inside result: {}".format(None))
+        result = None
         return(None)
+    finally:
+            print("Inside result: {}".format(result))
