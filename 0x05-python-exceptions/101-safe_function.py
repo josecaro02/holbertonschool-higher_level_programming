2d566191374bd30bcb1d194e4da04ce9966a5343
Jose David Caro <j.caro0207@gmail.com>
2020-01-08 21:09:06 +0000

    [Change fct, instead of recieve args[1] and args[2], recieve *args]

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
         return(result)
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
