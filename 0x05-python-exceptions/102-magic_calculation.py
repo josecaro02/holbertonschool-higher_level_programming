4d065b2c2c3737075ed8fbc2d7f9eb4b6a6c9e34
Jose David Caro <j.caro0207@gmail.com>
2020-01-08 23:06:47 +0000

    [Adding taks 102 magic calculation]

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
+            else:
+                result += ((a ** b) / i)
+        except:
+            result = a + b
+            break
+    return(result)
