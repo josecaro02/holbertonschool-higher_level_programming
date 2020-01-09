b644b66e2ae7b252a8efae0cb36c726390ec401e
josecaro02 <j.caro0207@gmail.com>
2020-01-09 23:25:11 +0000

    [Fix task 6]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,8 @@
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-            self.__size = size
-            self.__position = position
+            self.size = size
+            self.position = position
 
     @property
     def size(self):
