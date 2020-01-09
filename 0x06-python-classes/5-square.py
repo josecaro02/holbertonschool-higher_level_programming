4f2fd72718639896102684fac9d5564b50e75696
josecaro02 <j.caro0207@gmail.com>
2020-01-09 22:38:35 +0000

    [Adding option in method print when size is 0]

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,7 +22,10 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
