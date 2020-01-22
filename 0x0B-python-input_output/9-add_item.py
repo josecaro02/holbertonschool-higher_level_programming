2f050b8e6b5b4d086f9ccbe9bf7216a479b754e4
Jose David Caro <j.caro0207@gmail.com>
2020-01-22 01:24:08 +0000

    [Fix error, when file doesn't exist and try to assign arguments]

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,9 +7,9 @@ file_name = "add_item.json"
 len_argv = len(sys.argv)
 try:
     my_list = load_from_json_file("add_item.json")
-    for i in range(1, len_argv):
-        my_list.append(sys.argv[i])
 except:
     my_list = []
 finally:
+    for i in range(1, len_argv):
+        my_list.append(sys.argv[i])
     save_to_json_file(my_list, "add_item.json")
