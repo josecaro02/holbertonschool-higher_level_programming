984e2e3da5f8b5c267e1b54c4239483b7d248081
Jose David Caro <j.caro0207@gmail.com>
2020-01-22 03:00:01 +0000

    [Add task 13]

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        diction = {}
+        if type(attrs) == list and (type(i) == str for i in attrs):
+            for i in attrs:
+                try:
+                    getattr(self, i)
+                    diction[i] = getattr(self, i)
+                except:
+                    continue
+        else:
+            diction['first_name'] = self.first_name
+            diction['last_name'] = self.last_name
+            diction['age'] = self.age
+        return(diction)
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
