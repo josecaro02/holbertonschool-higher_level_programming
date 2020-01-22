87477c4cc5d3e7f9be090c3e07c543cdabad39f0
Jose David Caro <j.caro0207@gmail.com>
2020-01-22 00:43:04 +0000

    [Reupload task 9 with correct names of functions]

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    load_from_json = __import__('8-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('8-load_from_json_file')\
+        .load_from_json_file
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
     file_name = "add_item.json"
     len_argv = len(sys.argv)
     try:
-        my_list = load_from_json("add_item.json")
+        my_list = load_from_json_file("add_item.json")
         for i in range(1, len_argv):
             my_list.append(sys.argv[i])
         save_to_json_file(my_list, "add_item.json")
