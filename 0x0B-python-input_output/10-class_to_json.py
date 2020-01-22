ad1a491e82d388e921cc3339cb294df012d0bdfe
Jose David Caro <j.caro0207@gmail.com>
2020-01-22 02:18:42 +0000

    [Upload task 10]

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+
+
+def class_to_json(obj):
+    priv_att = '_' + obj.__class__.__name__
+    test = {}
+    for name in dir(obj):
+        if name.startswith('_') and not name.startswith(priv_att):
+            continue
+        else:
+            att = getattr(obj, name)
+            if type(att) == int or type(att) == bool or type(att) == str\
+               or type(att) == list or type(att) == dict:
+                test[name] = att
+    return (test)
