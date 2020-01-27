946cc481398418ed195caf1d35cfbaf5f1c72422
Jose David Caro <j.caro0207@gmail.com>
2020-01-27 15:17:45 -0500

    [Add check if list is empty in to_json_string method]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ method that convert a dictionary to a json"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return json.dumps(list_dictionaries)
 
