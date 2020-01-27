99f5251a4296c96379d2fb9ae3d59fac74439c50
Jose David Caro <j.caro0207@gmail.com>
2020-01-27 15:29:39 -0500

    [Change way to check if list exists]

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ method that convert a dictionary to a json"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if not list_dictionaries:
             msg_rtn = "[]"
             return (msg_rtn)
         return json.dumps(list_dictionaries)
