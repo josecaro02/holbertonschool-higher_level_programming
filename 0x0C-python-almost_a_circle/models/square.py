875d2b4450b7ba59ae2934b111417e2327aa6260
Jose David Caro <j.caro0207@gmail.com>
2020-01-27 13:56:00 -0500

    Fix setter size in square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ __init__ method """
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
