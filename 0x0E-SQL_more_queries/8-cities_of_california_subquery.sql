0a30b219f6af30a4cc0dc447bdb71287bd5dc0fa
Jose David Caro <j.caro0207@gmail.com>
2020-03-03 18:41:26 -0500

    [Adding task 8]

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- Lists all the cities of California thah can be found in
+-- database hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id =
+       (SELECT id FROM states
+       	       WHERE name = 'California');
