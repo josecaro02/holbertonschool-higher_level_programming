e2f4fe1b3f4a499087bbdccdff2fcd1b44ffab12
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 14:51:46 -0500

    [Fix pep 8 in task 0]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     sql = "SELECT * FROM states"
     cur.execute(sql)
