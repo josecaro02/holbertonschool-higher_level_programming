0c9b52eb0522044ebba9bebe78678fc2e3d03892
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 17:57:20 -0500

    [Add model state for task 6]

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Create model state with sqlAlchemy '''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    ''' Class state to save in database '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
