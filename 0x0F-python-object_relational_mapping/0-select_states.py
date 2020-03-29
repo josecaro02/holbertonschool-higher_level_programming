b255e10b0986d58c9500810882c5e19fe0533f0b
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 14:59:03 -0500

    [Change shebang]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+''' Prints table states in a database given '''
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     sql = "SELECT * FROM states ORDER BY id ASC"
     cur.execute(sql)
