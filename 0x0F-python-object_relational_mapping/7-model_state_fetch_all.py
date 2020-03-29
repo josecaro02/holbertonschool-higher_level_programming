c3b68bdade31968645dad4cd793553ffe4b7f54d
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 18:22:14 -0500

    Add declaration to avoid exectuion when import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,11 +6,12 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query(State).order_by(State.id):
-    print("{}: {}".format(instance.id, instance.name))
-session.close()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
+    session.close()
