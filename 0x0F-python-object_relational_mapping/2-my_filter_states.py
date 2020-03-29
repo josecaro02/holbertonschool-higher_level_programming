645a0ad9a30edb5c0338ad9f9250fd9db964bd30
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 15:22:13 -0500

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Prints states that matches with 'Arizona' in a database given '''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE 'Arizona' ORDER BY id ASC"
+    cur.execute(sql)
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    db.close()
