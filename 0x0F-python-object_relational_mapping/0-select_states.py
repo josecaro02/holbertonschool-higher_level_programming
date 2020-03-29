2ffc3763aca8162dce85b74132c74df1393aa5fd
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 14:54:09 -0500

    [Add code to specify that result must be order by id]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    sql = "SELECT * FROM states"
+    sql = "SELECT * FROM states ORDER BY id ASC"
     cur.execute(sql)
     result = cur.fetchall()
     for row in result:
