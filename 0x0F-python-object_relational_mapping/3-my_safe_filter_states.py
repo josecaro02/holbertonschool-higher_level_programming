4c0cdd9203720e9b9680a585eddc5ddb97e70071
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 15:59:10 -0500

    [Fix pep8 in task 3]

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    name_state= (sys.argv[4], )
+    name_state = (sys.argv[4], )
     cur.execute(sql, name_state)
     result = cur.fetchall()
     for row in result:
