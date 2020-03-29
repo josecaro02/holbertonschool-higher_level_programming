4a758819ef412696d6fbb0ee0bd9f658c79a3689
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 14:50:10 -0500

    [Fix code in task 0]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 import MySQLdb
 import sys
 
-if __name == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
