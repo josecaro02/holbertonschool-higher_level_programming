7723116fd84eb8128e397280bd2ea243e5d932c8
Jose David Caro <j.caro0207@gmail.com>
2020-03-30 00:03:01 -0500

    [Add collide to task 2]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    sql = "SELECT * FROM states WHERE name = '{}'\
+    COLLATE latin1_general_cs ORDER BY id ASC"
     cur.execute(sql.format(sys.argv[4]))
     result = cur.fetchall()
     for row in result:
