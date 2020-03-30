3663ee92d39a6acd28cf6f3f02204d0ae5caa9a2
Jose David Caro <j.caro0207@gmail.com>
2020-03-29 21:17:03 -0500

    [Fix code in task 12; change 2nd row name]

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = session.query(State).first()
-    new_state.name = 'Boyaca'
+    new_state = session.query(State).get(2)
+    new_state.name = 'New Mexico'
     session.commit()
     session.close()
