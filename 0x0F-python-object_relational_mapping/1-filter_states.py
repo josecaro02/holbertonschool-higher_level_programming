3aac9cbddd20b7b7dd88339e6f40ae3ac36dc1f6
Jose David Caro <j.caro0207@gmail.com>
2020-03-30 00:00:56 -0500

    [Add collide]

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    sql = "SELECT * FROM states WHERE name COLLATE latin1_general_cs\
+    LIKE 'N%' ORDER BY id ASC"
     cur.execute(sql)
     result = cur.fetchall()
     for row in result:
