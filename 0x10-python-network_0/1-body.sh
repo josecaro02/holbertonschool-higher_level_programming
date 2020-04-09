8c178260993f89de0540c9e7cac2af08f9d74bb1
Jose David Caro <j.caro0207@gmail.com>
2020-04-09 14:23:10 -0500

    Fix task 0-1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Prints response of a get request if http_code is 200
-status_code=$(curl -w "%{Http_code}" -s -o /dev/null -L "$1")
-if [ "$status_code" -eq 200 ]
-then
-    curl -sL "$1"
-fi
+curl -sL "$1"
