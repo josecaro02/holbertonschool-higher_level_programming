33565d246f87a5e7ad30beba62c99b598bed31c8
Jose David Caro <j.caro0207@gmail.com>
2020-04-15 13:16:27 -0500

    [Update task 0, 4 and 5]

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: OK")
+    print("\t- content: {}".format(r.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,4 +5,4 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
