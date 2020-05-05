76c6c460f81ffec6e3f29be1d0fda0ea3f736e84
Jose David Caro <j.caro0207@gmail.com>
2020-05-05 16:04:57 -0500

    [Reorganize if statements]

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const myArgs = process.argv;
-if (myArgs.length === 3) {
-  console.log('Argument Found');
-} else if (myArgs.length > 3) {
-  console.log('Arguments found');
-} else {
+if (myArgs.length === 2) {
   console.log('No argument');
+} else if (myArgs.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
