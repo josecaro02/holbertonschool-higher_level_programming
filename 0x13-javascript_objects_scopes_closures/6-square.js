2e5b434de2d95bd761726c93c337d43cd6a0016c
Jose David Caro <j.caro0207@gmail.com>
2020-05-11 19:58:28 -0500

    [Fix semistandard 'useless constructor' error]

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,10 +3,6 @@
 const cuadrado = require('./5-square');
 
 class Square extends cuadrado {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
