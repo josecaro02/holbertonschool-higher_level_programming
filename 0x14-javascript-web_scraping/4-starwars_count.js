dfaf2f1147efd121e3ff8620222908d82281e2b6
Jose David Caro <j.caro0207@gmail.com>
2020-05-12 18:58:58 -0500

    [Set variable inside function]

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@
 const request = require('request');
 const myArgs = process.argv;
 const url = myArgs[2];
-let count = 0;
 
 const options = {
   url: url,
@@ -13,8 +12,9 @@ const options = {
 };
 
 function callback (error, response, body) {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     const info = JSON.parse(body);
+    let count = 0;
     for (let i = 0; i < info.count; i++) {
       const characters = info.results[i].characters;
       for (let j = 0; j < characters.length; j++) {
