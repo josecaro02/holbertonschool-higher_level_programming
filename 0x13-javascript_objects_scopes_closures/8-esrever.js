710a9452b9f0fc2445fcf043f2b96de9f0069acb
Jose David Caro <j.caro0207@gmail.com>
2020-05-11 20:32:43 -0500

    [Add task 8]

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const newArray = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    newArray.push(list[i]);
+  }
+  return (newArray);
 };
