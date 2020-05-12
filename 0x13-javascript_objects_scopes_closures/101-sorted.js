32820704bf3df06f9eacf3fdaaaff903a4c8f9ce
Jose David Caro <j.caro0207@gmail.com>
2020-05-11 22:55:07 -0500

    Add task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value]) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [];
+    newDict[value].push(key);
+  }
+}
+console.log(newDict);
