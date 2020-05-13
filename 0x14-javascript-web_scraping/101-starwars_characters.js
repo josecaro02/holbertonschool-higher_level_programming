ea25b7e33fc49d781e5200fbdb9eac0d397bdf0f
Jose David Caro <j.caro0207@gmail.com>
2020-05-12 22:39:40 -0500

    [Upload task 101]

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,8 +2,8 @@
 
 const request = require('request');
 const myArgs = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films';
-
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[2];
+const orderDict = {};
 const options = {
   url: url,
   headers: {
@@ -14,23 +14,27 @@ const options = {
 function callback (error, response, body) {
   if (!error && response.statusCode === 200) {
     const info = JSON.parse(body);
-    for (let i = 0; i < info.count; i++) {
-      if (info.results[i].episode_id === parseInt(myArgs[2])) {
-        const characters = info.results[i].characters;
-        for (let k = 0; k < characters.length; k++) {
-          const name = characters[k];
-          request(name, function (err, response, body) {
-            if (err) {
-              console.log(err);
-            } else {
-              const resultName = JSON.parse(body);
-              console.log(resultName.name);
-            }
-          });
+    const characters = info.characters;
+    for (let k = 0; k < characters.length; k++) {
+      const name = characters[k];
+      request(name, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          const resultName = JSON.parse(body);
+          saveNames(name.split('/')[5], resultName.name, characters.length);
         }
-      }
+      });
     }
   }
 }
 
+function saveNames (id, value, size) {
+  orderDict[id] = value;
+  if (Object.entries(orderDict).length === size) {
+    for (const key in orderDict) {
+      console.log(orderDict[key]);
+    }
+  }
+}
 request(options, callback);
