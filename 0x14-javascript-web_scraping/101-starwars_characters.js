2e539026ee200631ee9c07a38431e63a861731eb
Jose David Caro <j.caro0207@gmail.com>
2020-05-12 21:38:35 -0500

    [Fix task 101]

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const myArgs = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[2];
+const url = 'https://swapi-api.hbtn.io/api/films';
 
 const options = {
   url: url,
@@ -14,17 +14,21 @@ const options = {
 function callback (error, response, body) {
   if (!error && response.statusCode === 200) {
     const info = JSON.parse(body);
-    const characters = info.characters;
-    for (let k = 0; k < characters.length; k++) {
-      const name = characters[k];
-      request(name, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          const resultName = JSON.parse(body);
-          console.log(resultName.name);
+    for (let i = 0; i < info.count; i++) {
+      if (info.results[i].episode_id === parseInt(myArgs[2])) {
+        const characters = info.results[i].characters;
+        for (let k = 0; k < characters.length; k++) {
+          const name = characters[k];
+          request(name, function (err, response, body) {
+            if (err) {
+              console.log(err);
+            } else {
+              const resultName = JSON.parse(body);
+              console.log(resultName.name);
+            }
+          });
         }
-      });
+      }
     }
   }
 }
