44bcca2c046b609e725a04f020fd29b05b3c8ae9
Jose David Caro <j.caro0207@gmail.com>
2020-05-12 19:20:05 -0500

    [Add task 5]

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const myArgs = process.argv;
+const url = myArgs[2];
+
+const options = {
+  url: url,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+function callback (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const info = body;
+    console.log(info);
+    fs.appendFile(myArgs[3], info, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+}
+request(options, callback);
